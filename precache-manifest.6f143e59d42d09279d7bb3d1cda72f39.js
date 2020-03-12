b1db682298f16e666df41bb1487f981c097ca256
Srijan <chapssrijan@gmail.com>
2020-03-12 16:29:17 +0200

    Updates

---
+++ precache-manifest.6f143e59d42d09279d7bb3d1cda72f39.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac92c5880fff4e4099b0b37a6061437",
+    "url": "/my-blog/index.html"
+  },
+  {
+    "revision": "d9fc84aac8cc69790071",
+    "url": "/my-blog/static/css/main.a0f90f33.chunk.css"
+  },
+  {
+    "revision": "85d72c6c7fd7d037cda1",
+    "url": "/my-blog/static/js/2.932d1ce5.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/my-blog/static/js/2.932d1ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9fc84aac8cc69790071",
+    "url": "/my-blog/static/js/main.58a98a0b.chunk.js"
+  },
+  {
+    "revision": "971b58f4fb4fd45ecd79",
+    "url": "/my-blog/static/js/runtime-main.cf125f2f.js"
+  },
+  {
+    "revision": "fd7a55fd02a4b10d24d2ec886c6fd09c",
+    "url": "/my-blog/static/media/auction.fd7a55fd.PNG"
+  },
+  {
+    "revision": "783c2b974cd8f93764271627a6c66174",
+    "url": "/my-blog/static/media/cover-removebg.783c2b97.png"
+  },
+  {
+    "revision": "12c26a26be2ff41c9edf7fef333bbfe9",
+    "url": "/my-blog/static/media/stock.12c26a26.png"
+  }
+]);
