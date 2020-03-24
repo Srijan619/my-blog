5df1a241d63b32d7392e4c8b8c7a5015a9dced57
Srijan619 <chapssrijan@gmail.com>
2020-03-24 13:16:00 +0200

    Page Navigation Fixed

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -89,6 +89,7 @@ class index extends Component {
     render() {
         const { classes } = this.props;
         const { blogData } = this.state;
+        console.log(blogData)
         return (
             <div className={classes.container}>
                 <ExpansionPanel className={classes.expansion} >
@@ -124,7 +125,7 @@ class index extends Component {
                                         <IconButton >
 
                                             <MoreIcon />
-                                            <Link to='/my-blogs/Blogs' target="_blank" style={{ textDecoration: 'none' }}>
+                                            <Link to='/my-blog/Blogs' target="_blank" style={{ textDecoration: 'none' }}>
                                                 <Typography className={classes.footNote} style={{ paddingLeft: 5 }} >See all posts</Typography>
                                             </Link>
                                         </IconButton>
